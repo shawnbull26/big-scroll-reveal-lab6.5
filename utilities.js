5dedfc6db02fa0e6644ecfe3e078b6a37ab0b893
Daeshawn Bullock <75647362+shawnbull26@users.noreply.github.com>
2026-02-22 20:09:44 +0000

    made changes to html, css, and js file

---
+++ utilities.js
@@ -0,0 +1,5 @@
+ScrollReveal().reveal('#test1', { delay: 500 }, { easing: 'ease-in-out' });
+ScrollReveal().reveal('#test2', { delay: 1000 }, { easing: 'ease-in-out' });
+ScrollReveal().reveal('#test3', { delay: 1500 }, { easing: 'ease-in-out' });
+ScrollReveal().reveal('#test4', { delay: 2000 }, { easing: 'ease-in-out' });
+ScrollReveal().reveal('#test5', { delay: 2500 }, { easing: 'ease-in-out' });
